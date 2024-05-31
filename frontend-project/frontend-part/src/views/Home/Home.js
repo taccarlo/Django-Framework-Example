bd73f4240240d71942123948a179b4a0bd0c59e3
Carlo Tacchella <tacchella.carlo@gmail.com>
2024-05-31 19:36:06 +0200

    lint

--- frontend-project/frontend-part/src/views/Home/Home.js
+++ frontend-project/frontend-part/src/views/Home/Home.js
@@ -1,48 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
+function HomePage() {
+  const [dateFrom, setDateFrom] = useState(null);
+  const [dateTo, setDateTo] = useState(null);
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <h1>Subscription Viewer</h1>
+        <fieldset className="form-group">
+          <input
+            className="form-control"
+            type="date"
+            placeholder="Data di inizio"
+            onChange={(event) => {
+              setDateFrom(event.target.value);
+            }}
+          />
+        </fieldset>
+        <fieldset className="form-group">
+          <input
+            className="form-control"
+            type="date"
+            placeholder="Data di fine"
+            onChange={(event) => {
+              setDateTo(event.target.value);
+            }}
+          />
+        </fieldset>
 
-function HomePage(){
-    const [datefrom, setdatefrom] = useState(null);
-    const [dateto, setdateto] = useState(null);
-    return <>
-        <div>
-
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control"
-                      type="date"
-                      placeholder="Data di inizio"
-                      onChange={
-                        (event) => {
-                            setdatefrom(event.target.value);
-                        }
-                      }
-                    />
-                  </fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control"
-                      type="date"
-                      placeholder="Data di fine"
-                      onChange={
-                        (event) => {
-                            setdateto(event.target.value);
-                        }
-                      }
-                    />
-                  </fieldset>
-        </div>
-
-        {
-          (datefrom&&dateto)?
-            <Link to={"/list/"+datefrom+"/"+dateto} >Vai alla lista</Link>
-            :
-            <h3>Inserire le date desiderate</h3>
-        }
-   
-    </>;
+        {dateFrom && dateTo ? (
+          <h3>
+            <Link to={"/list/" + dateFrom + "/" + dateTo}>Vai alla lista</Link>
+          </h3>
+        ) : (
+          <h3>Inserire le date desiderate</h3>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default HomePage;
-
--- frontend-project/frontend-part/src/views/List/List.js
+++ frontend-project/frontend-part/src/views/List/List.js
@@ -1,18 +1,17 @@
-
-import {Link,useParams} from 'react-router-dom';
-import { useState,useEffect,useLocation} from 'react';
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect, useLocation } from "react";
 import ListItem from "./components/ListItem";
-import axios from 'axios';
+import axios from "axios";
 
 //import axios from 'axios';
 //todo: if i don't have data load Home
 //moment(moment('2020-11-18', 'YYYY-MM-DD')).format('DD-MM-YYYY');
 
-function ListPage(){
-    const params = useParams();
-    //const { state } = props.location;
-    //const { name, age } = state;
-    /*useEffect(() => {
+function ListPage() {
+  const params = useParams();
+  //const { state } = props.location;
+  //const { name, age } = state;
+  /*useEffect(() => {
         // POST request using axios with error handling
         const article = { title: 'React POST Request Example' };
         axios.post('https://reqres.in/invalid-url', article)
@@ -27,11 +26,15 @@ function ListPage(){
     <p>Name: {name}</p>
     <p>Age: {age}</p>*/
 
-    return <>
-    <h1>lista elementi da {params.dateFrom} a {params.dateTo}</h1>
-        <ListItem titolo="ciao" ammontare="salve"></ListItem>
-        <ListItem titolo="buo" ammontare="ngiorno"></ListItem>
-        <Link to="/">To Home</Link>
-    </>;
+  return (
+    <>
+      <h1>
+        lista elementi da {params.dateFrom} a {params.dateTo}
+      </h1>
+      <ListItem titolo="ciao" ammontare="salve"></ListItem>
+      <ListItem titolo="buo" ammontare="ngiorno"></ListItem>
+      <Link to="/">To Home</Link>
+    </>
+  );
 }
 export default ListPage;
